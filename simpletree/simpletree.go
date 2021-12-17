b7f530c331ba4759af8cb56c8fa908340d637efb
Leonardone <neetsdkasu@gmail.com>
2021-12-17 17:19:44 +0900

    simpletreeのコメント書いた

--- simpletree/simpletree.go
+++ simpletree/simpletree.go
@@ -1,6 +1,42 @@
 // Author: Leonardone @ NEETSDKASU
 // License: MIT
 
+// github.com/neetsdkasu/avltreeのRealTree,RealNodeの実装例
+// 実装に必要な最小限の構成になっている
+//
+// コード例
+//
+//		import (
+//			"fmt"
+//			"github.com/neetsdkasu/avltree"
+//			. "github.com/neetsdkasu/avltree/intkey"
+//			"github.com/neetsdkasu/avltree/simpletree"
+//		)
+//		func Example() {
+//			tree := simpletree.New(false)
+//			avltree.Insert(tree, false, IntKey(12), 345)
+//			avltree.Insert(tree, false, IntKey(67), 890)
+//			avltree.Insert(tree, false, IntKey(333), 666)
+//			avltree.Insert(tree, false, IntKey(-5), 12345)
+//			avltree.Delete(tree, IntKey(67))
+//			avltree.Update(tree, IntKey(333), func(key avltree.Key, oldValue interface{}) (newValue interface{}, keepOldValue bool) {
+//				newValue = oldValue.(int) * 3
+//				return
+//			})
+//			if node := avltree.Find(tree, IntKey(12)); node != nil {
+//				fmt.Println("Find!", node.Key(), node.Value())
+//			}
+//			avltree.Iterate(tree, false, func(node avltree.Node) (breakIteration bool) {
+//				fmt.Println("Iterate!", node.Key(), node.Value())
+//				return
+//			})
+//			// Output:
+//			// Find! 12 345
+//			// Iterate! -5 12345
+//			// Iterate! 12 345
+//			// Iterate! 333 1998
+//		}
+//
 package simpletree
 
 import "github.com/neetsdkasu/avltree"
