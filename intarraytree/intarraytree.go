8c46f9f9bc86f1aaafc608d4d1423dd938660c46
Leonardone <neetsdkasu@gmail.com>
2021-12-17 16:58:12 +0900

    intarraytreeのコメント書いた

--- intarraytree/intarraytree.go
+++ intarraytree/intarraytree.go
@@ -1,6 +1,61 @@
 // Author: Leonardone @ NEETSDKASU
 // License: MIT
 
+// github.com/neetsdkasu/avltreeのRealTree,RealNodeの実装例
+// int型の可変長配列(スライス)上にAVL木を構築する
+//
+// 扱えるキーはintkeyのIntKeyのみ
+// 扱える値はint型のみ
+//
+// int型は32bitマシンだと4bytes、64bitマシンだと8bytesになるので実行環境によって必要メモリ量が変わる
+//
+// 配列の最初の３要素は以下の木に関する情報を保持
+// + ルートノードのインデックス
+// + 同一キーを許可するかどうかの値
+// + 再利用可能なノードのインデックス
+//
+// １つのノードは７個分の要素で構成され、以下の情報を保持
+// + 左の子ノードのインデックス
+// + 右の子ノードのインデックス
+// + 木におけるノードの高さ
+// + 親ノードのインデックス
+// + 左右の子孫も合わせたノード総数
+// + ノードのキー
+// + ノードの値
+//
+// コード例
+//
+//		import (
+//			"fmt"
+//			"github.com/neetsdkasu/avltree"
+//			"github.com/neetsdkasu/avltree/intarraytree"
+//			. "github.com/neetsdkasu/avltree/intkey"
+//		)
+//		func Example_intarraytree() {
+//			tree := intarraytree.New(false)
+//			avltree.Insert(tree, false, IntKey(12), 345)
+//			avltree.Insert(tree, false, IntKey(67), 890)
+//			avltree.Insert(tree, false, IntKey(333), 666)
+//			avltree.Insert(tree, false, IntKey(-5), 12345)
+//			avltree.Delete(tree, IntKey(67))
+//			avltree.Update(tree, IntKey(333), func(key avltree.Key, oldValue interface{}) (newValue interface{}, keepOldValue bool) {
+//				newValue = oldValue.(int) * 3
+//				return
+//			})
+//			if node := avltree.Find(tree, IntKey(12)); node != nil {
+//				fmt.Println("Find!", node.Key(), node.Value())
+//			}
+//			avltree.Iterate(tree, false, func(node avltree.Node) (breakIteration bool) {
+//				fmt.Println("Iterate!", node.Key(), node.Value())
+//				return
+//			})
+//			// Output:
+//			// Find! 12 345
+//			// Iterate! -5 12345
+//			// Iterate! 12 345
+//			// Iterate! 333 1998
+//		}
+//
 package intarraytree
 
 import (
