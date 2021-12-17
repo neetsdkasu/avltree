a6aae6c3a97368cbc9d0d9849ad33ccf0a8b844c
Leonardone <neetsdkasu@gmail.com>
2021-12-17 22:42:22 +0900

    stringkeyのコメント書いた

--- stringkey/stringkey.go
+++ stringkey/stringkey.go
@@ -1,6 +1,9 @@
 // Author: Leonardone @ NEETSDKASU
 // License: MIT
 
+// github.com/neetsdkasu/avltreeのKeyの実装例
+// string型をそのままキーにしてある
+// 標準パッケージのstrings.Compareの結果をそのままキーの比較の値として使っている
 package stringkey
 
 import (
